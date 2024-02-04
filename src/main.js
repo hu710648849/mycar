d83a7ead36629c175c193607cabef8efb6c7620e
“hu710648849” <710648849@qq.com>
2024-02-04 20:48:37 +0800

    调整项目目录

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import './assets/main.css'
-// console.log(gaga)
-// consoel.log(smsm)
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import '@/assets/main.scss'
 
 import App from './App.vue'
 import router from './router'
@@ -11,5 +9,4 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
