63dad789aaaf9cc368478e9147ee5bc5f29abf1e
“hu710648849” <710648849@qq.com>
2024-02-04 22:42:39 +0800

    引入了Element-Plus

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 
 <template>
   <div>我是Div</div>
+  <button type="button">按钮1</button>
+  <button type="button">按钮2</button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
 </template>
 
 <style scoped></style>
