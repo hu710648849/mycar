faeb10717c643f1b4954ff0f949fb95119b73cac
“hu710648849” <710648849@qq.com>
2024-02-05 15:35:56 +0800

    pinia构建模块

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
+// // 导入pinia持久化插件
+// import persist from 'pinia-plugin-persistedstate'
+import pinia from './stores/index'
 import '@/assets/main.scss'
 
 import App from './App.vue'
@@ -7,6 +10,7 @@ import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+// app.use(createPinia().use(persist))
+app.use(pinia) // pinia独立维护
 app.use(router)
 app.mount('#app')
